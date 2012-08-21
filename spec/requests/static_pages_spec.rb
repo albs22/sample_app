1084b3ceaceea9f8df89c661f6fcfc02770ddb67
Alex <a.behlen@gmail.com>
2012-08-20 22:58:56 -0700

    5.2.2 update custom css and added contact page and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -36,5 +36,18 @@ describe "StaticPages" do
       page.should have_content('About Us')
     end
   end
+
+  describe "Contact Page" do
+    
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
 
