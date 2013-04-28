d4ac1caff49b3376e32cb422251842997b505d4a
albs22 <a.behlen@gmail.com>
2013-04-28 10:58:24 -0700

    Diplay micropost and related tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,10 +30,9 @@ describe "User pages" do
     end
   end
   
-   describe "delete links" do
+  describe "delete links" do
 
       it { should_not have_link('delete') }
-     
 
       describe "as an admin user" do
         let(:admin) { FactoryGirl.create(:admin) }
@@ -43,8 +42,7 @@ describe "User pages" do
           visit users_path
         end
         
-        
-        it { should have_link('delete', href: user_path(:user)) }
+        it { should have_link('delete', href: user_path(User.first)) }
         it "should be able to delete another user" do
           expect { click_link('delete') }.to change(User, :count).by(-1)
         end
@@ -147,4 +145,24 @@ describe "User pages" do
       specify { user.reload.email.should == new_email }
     end
   end
+  
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end
+  
+  
+  
 end
